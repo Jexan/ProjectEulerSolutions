b6cbc97b07a9136c5602ba97d8117f85528d5d38
Jexan <Jexan13@gmail.com>
2018-08-14 23:55:10 -0400

    Fix the test runner to make it more versatile

--- tests/test_complete_ok.py
+++ tests/test_complete_ok.py
@@ -1,7 +1,15 @@
 import unittest
 import Complete as C
 
+TESTING_NOW = 1
+
 class TestCompleteChallengesYieldCorrectResults(unittest.TestCase):
-    def test_assure_1_ok(self):
-        import Complete.E001
-        self.assertEqual(Complete.E001.result, 233168) 
+    def test_assure_given_ok(self):
+        string = str(TESTING_NOW)
+        n_zeroes = '0' * (3 - len(string))
+
+        eval('assure_{}(self)'.format(n_zeroes + string))
+
+def assure_001(self):
+    from Complete.E001 import result
+    self.assertEqual(result, 233168)
